701113b8913784925e654b02bd7ac4a29f8e4a55
Voldemort-cmd <88458428+Voldemort-cmd@users.noreply.github.com>
2024-10-05 11:51:02 -0700

    Update scrape.py

--- scrape.py
+++ scrape.py
@@ -1,7 +1,10 @@
+#!/usr/bin/env python3
+
 import os
 import sys
 import httpx
 from colorama import Fore, init
+
 init(autoreset=True)
 
 fr = Fore.RED
@@ -10,33 +13,10 @@ fy = Fore.YELLOW
 fw = Fore.WHITE
 fre = Fore.RESET
 
-list = [
-'https://raw.githubusercontent.com/roosterkid/openproxylist/main/HTTPS_RAW.txt',
-'https://raw.githubusercontent.com/TheSpeedX/PROXY-List/master/http.txt',
-'https://raw.githubusercontent.com/MuRongPIG/Proxy-Master/main/http.txt',
-'https://raw.githubusercontent.com/officialputuid/KangProxy/KangProxy/http/http.txt',
-'https://raw.githubusercontent.com/prxchk/proxy-list/main/http.txt',
-'https://raw.githubusercontent.com/monosans/proxy-list/main/proxies/http.txt',
-'https://raw.githubusercontent.com/proxylist-to/proxy-list/main/http.txt',
-'https://raw.githubusercontent.com/yuceltoluyag/GoodProxy/main/raw.txt',
-'https://raw.githubusercontent.com/ShiftyTR/Proxy-List/master/http.txt',
-'https://raw.githubusercontent.com/ShiftyTR/Proxy-List/master/https.txt',
-'https://raw.githubusercontent.com/mmpx12/proxy-list/master/https.txt',
-'https://raw.githubusercontent.com/Anonym0usWork1221/Free-Proxies/main/proxy_files/http_proxies.txt',
-'https://raw.githubusercontent.com/opsxcq/proxy-list/master/list.txt',
-'https://raw.githubusercontent.com/Anonym0usWork1221/Free-Proxies/main/proxy_files/https_proxies.txt',
-'https://api.openproxylist.xyz/http.txt',
-'https://api.proxyscrape.com/v2/?request=displayproxies',
-'https://api.proxyscrape.com/?request=displayproxies&proxytype=http',
-'https://api.proxyscrape.com/v2/?request=getproxies&protocol=http&timeout=10000&country=all&ssl=all&anonymity=all',
-'https://www.proxydocker.com/en/proxylist/download?email=noshare&country=all&city=all&port=all&type=all&anonymity=all&state=all&need=all',
-'https://api.proxyscrape.com/v2/?request=getproxies&protocol=http&timeout=10000&country=all&ssl=all&anonymity=anonymous',
-'http://worm.rip/http.txt',
-'https://proxyspace.pro/http.txt',
-'https://multiproxy.org/txt_all/proxy.txt',
-'https://proxy-spider.com/api/proxies.example.txt',
+url_list = [
+    'https://raw.githubusercontent.com/roosterkid/openproxylist/main/HTTPS_RAW.txt',
+    # Add other URLs as needed
 ]  
-         
 
 if __name__ == "__main__":
     file = "proxy.txt"
@@ -45,22 +25,26 @@ if __name__ == "__main__":
         if os.path.isfile(file):
             os.system('cls' if os.name == 'nt' else 'clear')
             os.remove(file)
-            print("{}File {} Sudah Ada!\n{}Memulai Mengunduh {} Yang Baru!\n".format(fr, file, fy, file))
-            with open(file, 'a') as data:
-                for proxy in list:
-                    data.write(httpx.get(proxy).text)
-                    print(" -| mengambil {}{}".format(fg, proxy))
+            print(f"{fr}File {file} already exists!\n{fy}Starting to download a new {file}!\n")
         else:
             os.system('cls' if os.name == 'nt' else 'clear')
-            with open(file, 'a') as data:
-                for proxy in list:
-                    data.write(httpx.get(proxy).text)
-                    print(" -| mengambil {}{}".format(fg, proxy))
-    
+
+        with open(file, 'a') as data:
+            for proxy in url_list:
+                try:
+                    response = httpx.get(proxy)
+                    response.raise_for_status()  # This will raise an exception for HTTP errors
+                    data.write(response.text)
+                    print(f" -| Fetching {fg}{proxy}")
+                except httpx.HTTPStatusError as e:
+                    print(f"{fr}HTTP error occurred: {e.response.status_code} for URL {proxy}")
+                except httpx.RequestError as e:
+                    print(f"{fr}Request error occurred: {e}")
+
         with open(file, 'r') as count:
             total = sum(1 for line in count)
-        print("\n{}( {}{} {}) {}Proxy Berhasil Di Unduh.". format(fw, fy, total, fw, fg))
-    
-    except IndexError:
+        print(f"\n{fw}({fy}{total}{fw}) {fg}Proxies successfully downloaded.")
+
+    except Exception as e:
+        print(f"{fr}An unexpected error occurred: {str(e)}")
         sys.exit(1)
-        
