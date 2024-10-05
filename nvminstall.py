06a010c93262cc69b2b4adf9be2543c8b3f45047
Voldemort-cmd <88458428+Voldemort-cmd@users.noreply.github.com>
2024-10-05 11:59:43 -0700

    Update nvminstall.py

--- nvminstall.py
+++ nvminstall.py
@@ -1,13 +1,28 @@
+#!/usr/bin/env python3
+
 import os
 import sys
 
+# Update and install required packages for Kali Linux
+os.system('sudo apt update -y && sudo apt install wget curl -y')
 
-os.system('pkg update -y && pkg install wget curl proot tar -y && wget https://raw.githubusercontent.com/AndronixApp/AndronixOrigin/master/Installer/Debian/debian.sh -O debian.sh && chmod +x debian.sh && bash debian.sh')
-os.system('apt install npm')
-os.system('sudo apt install curl ')
+# Download and run the Debian installation script from Andronix (if you still need it)
+os.system('wget https://raw.githubusercontent.com/AndronixApp/AndronixOrigin/master/Installer/Debian/debian.sh -O debian.sh && chmod +x debian.sh && bash debian.sh')
+
+# Install npm (node package manager)
+os.system('sudo apt install npm -y')
+
+# Install nvm (Node Version Manager)
 os.system('curl https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash')
-os.system('source ~/.bashrc')
-os.system('nvm install --lts')
+
+# Reload bash configuration to make nvm available
 os.system('source ~/.bashrc')
 
+# Install the LTS version of Node.js using nvm
+os.system('nvm install --lts')
+
+# Reload bash configuration again to apply nvm changes
+os.system('source ~/.bashrc')
+
+# Success message
 print(f"\x1b[31m[\x1b[33mMedusa\x1b[31m]\x1b[0m \x1b[32m> \x1b[33mInstallation Successful")
